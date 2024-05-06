a89adc7904e08f2ae3fcdeb2cc4fea08303ece21
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-06 15:08:19 -0300

    Update paineis.py

--- paineis.py
+++ paineis.py
@@ -1,19 +1,29 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+#URL PARA ACESSO
 driver.get("https://loger.belgo.com.br/LOGER_WEB/painelPontoCarregamento.html")
 
-elemento_centro = driver.find_elements("codCentro")
+try:
+    wait= WebDriverWait(driver,120)
+    driver.implicitly_wait(10)
 
-ponto_carregamento = driver.find_elements("codPC")
+#PROCURA E INCLUI O VALOR
+    #elem= driver.find_element(By.ID, "codCentro").send_keys("9064")
+    elem1=driver.find_element(By.NAME, "codPC").send_keys("08")
+    elem2=driver.find_element(By.NAME, "pageSize").send_keys("20")
+    elem3=driver.find_element(By.NAME, "pageSize2").send_keys("20")
+    elem4=driver.find_element(By.NAME, "refreshTime").send_keys("10")
 
-numero_linha = driver.find_elements("pageSize")
+    #assert elem.__getattribute__("9064") == "9064"
+    assert elem1.__getattribute__("08") == "08"
+    assert elem2.__getattribute__("20") == "20"
+    assert elem3.__getattribute__("20") == "20"
+    assert elem4.__getattribute__("10") == "10"
 
-numero_linha_2 = driver.find_elements("")
-
-refresh = driver.find_elements("")
-
-apresen_painel = driver.find_elements("")
+finally:
+    driver.quit()
