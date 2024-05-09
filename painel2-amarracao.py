e478b0349be3d6e2395498c58f62c6d85682964a
Lucas Oliveira <95634605+Lucasvitorre@users.noreply.github.com>
2024-05-09 15:40:17 -0300

    Update painel2-amarracao.py

--- painel2-amarracao.py
+++ painel2-amarracao.py
@@ -11,36 +11,36 @@ driver.get("https://loger.belgo.com.br/LOGER_WEB/painelPontoCarregamento.html")
 
 elemento = driver.find_element(By.NAME,"codCentro")
 elemento.clear()
-time.sleep(2)
-elemento.send_keys()
+time.sleep(1)
+elemento.send_keys("9460")
 
 elemento1 = driver.find_element(By.NAME,"codPC")
 elemento1.clear()
-time.sleep(2)
+time.sleep(1)
 elemento1.send_keys()
 
 elemento2 = driver.find_element(By.NAME,"pageSize")
 elemento2.clear()
-time.sleep(2)
-elemento2.send_keys()
+time.sleep(1)
+elemento2.send_keys("20")
 
 elemento3 = driver.find_element(By.NAME,"pageSize2")
 elemento3.clear()
-time.sleep(2)
-elemento3.send_keys()
+time.sleep(1)
+elemento3.send_keys("20")
 
 elemento4 = driver.find_element(By.NAME,"refreshTime")
 elemento4.clear()
-time.sleep(2)
-elemento4.send_keys()
+time.sleep(1)
+elemento4.send_keys("10")
 
 elemento5 = driver.find_element(By.XPATH,'//*[@id="configuracao"]/table/tbody/tr/td/table/tbody/tr[8]/td[2]/input[3]')
-time.sleep(2)
+time.sleep(1)
 elemento5.click()
 
 
 elemento6 = driver.find_element(By.XPATH,'//*[@id="configuracao"]/table/tbody/tr/td/table/tbody/tr[10]/td/input')
-time.sleep(2)
+time.sleep(1)
 elemento6.click()
 
 driver.fullscreen_window()
